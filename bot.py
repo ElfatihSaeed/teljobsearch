5f614e4584ee2243b5c1201799582a327a625b80
ElfatihSaeed <elfatih.s@gmail.com>
2021-09-10 21:29:46 +0200

    removing schedule and daily report

--- bot.py
+++ bot.py
@@ -16,17 +16,9 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 DATABASE_URL = os.getenv('DATABASE_URL')
+wlcm_msg = "!\nWelcome to @sudanjobsearch_bot.\nWould you like to subscribe to weekly report?\n/Subscribe\n/help - Print help message"
 server = Flask(__name__)
 
-def get_subscribers():
-  # conn = sqlite3.connect('sudanjobsearch.db')
-  conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-  cur = conn.cursor()
-  cur.execute(f'select * from tbl_subscribers')
-  subs = cur.fetchall()
-  cur.close()
-  conn.close()
-  return subs
 
 def manage_sub(message,stmnt):
   # conn = sqlite3.connect('sudanjobsearch.db')
@@ -37,9 +29,6 @@ def manage_sub(message,stmnt):
   cur.close()
   conn.close()
 
-
-
-wlcm_msg = "!\nWelcome to @sudanjobsearch_bot.\nWould you like to subscribe to weekly report?\n/Subscribe\n/help - Print help message"
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -67,59 +56,6 @@ def ck1(message):
         manage_sub(message,mng_sub)
     bot.send_message(message.chat.id,f'"{message.text}"\nis your watch list now,\nto change please send /Change\nto unsubscribe please \nsend /Unsubscribe')
 
-def search_send_jobs(sub_id,keywords,sites):
-
-  for keyword in keywords:
-
-    if 'sudancareers' in sites:
-      jobs_list = ''
-      site_url  = f'https://www.sudancareers.com/job-vacancies-search-sudan/{keyword}?'
-      print(f'Searching sudancareers at {site_url}')
-      html_text = requests.get(site_url).text
-      soup = BeautifulSoup(html_text,'lxml')
-      jobs = soup.find_all('div',class_='job-description-wrapper',limit=1)
-      for job in jobs:
-        job_title = job.find('h5').text.strip()
-        job_comp_date = job.find('p',class_='job-recruiter').text.split('|')
-        job_post_date = job_comp_date[0]
-        company_name = job_comp_date[1]
-        location = job.find('div',class_='col-lg-5 col-md-5 col-sm-5 col-xs-12 job-title').text.split('\n')[4].replace('Region of : ','').strip()
-        job_details = 'https://www.sudancareers.com' + job.find('h5').a['href']
-        jobs_list +=  f'Job Title   : {job_title}\n' + \
-                      f'Company  : {company_name}\n' +  \
-                      f'Location    : {location}\n' + \
-                      f'<a href="{job_details}">More Details</a> \n'
-      print(jobs_list)
-      print(f'Done searching sudancareers\n')
-      if jobs_list != '':
-        try:
-          bot.send_message(sub_id,jobs_list,parse_mode = "html",disable_web_page_preview=True)
-        except :
-          print("Unexpected error:", sys.exc_info()[0],'\n')
-      
-    if 'orooma' in sites:
-      jobs_list = ''
-      site_url = f'https://orooma.com/jobs/all/?q={keyword}' 
-      print(f'Searching orooma at {site_url}')
-      html_text = requests.get(site_url).text
-      soup = BeautifulSoup(html_text,'lxml')
-      jobs = soup.find_all('div',class_='card_group_item job_group_item',limit=1)
-      for job in jobs:
-        company_name = job.find('div',class_='m_0 p_0').text.strip()
-        job_details = 'https://orooma.com' + job.find('a',class_='p_light overflow_auto d_block')['href']
-        job_title = job.find('h4',class_='text_primary m_0').text.strip()
-        jobs_list +=  f'Job Title   : {job_title}\n' + \
-                      f'Company  : {company_name}\n' +  \
-                      f'<a href="{job_details}">More Details</a> \n' 
-      print(jobs_list)
-      print(f'Done searching orooma\n')
-      if jobs_list != '':
-        try:
-          bot.send_message(sub_id,jobs_list,parse_mode = "html",disable_web_page_preview=True)
-        except :
-          print("Unexpected error:", sys.exc_info()[0],'\n')
-    
-  bot.send_message(sub_id,'To unsubscribe from weekly report\nsend /Unsubscribe\nto change watch list please\nsend /Change')      
 
 
 def scra_sites(message,sites):
@@ -182,21 +118,6 @@ def send_jobs(message):
   scra_sites(message,sites)
 
 
-def daily():
-  sites = ['sudancareers','orooma']
-  subs = get_subscribers()
-  for sub in subs:
-    sub_id = sub[0]
-    sub_jobs = sub[4].split(',')
-    print(sub_id,sub_jobs)
-    search_send_jobs(sub_id,sub_jobs,sites)
-
-
-schedule.every(1).minutes.do(daily)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
 
 @server.route('/' + API_KEY, methods=['POST'])
 def getMessage():
