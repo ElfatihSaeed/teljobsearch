c60538af674fa26efa3706cba158b0173b29c51e
ElfatihSaeed <elfatih.s@gmail.com>
2021-09-17 22:20:48 +0200

    adding chat log

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from flask import Flask, request
 import sqlite3
 import psycopg2
 import schedule
-import time
+from datetime import datetime
 
 load_dotenv()
 
@@ -17,6 +17,7 @@ API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 DATABASE_URL = os.getenv('DATABASE_URL')
 server = Flask(__name__)
+dtime = datetime.now()
 
 wlcm_msg = "Welcome to @sudanjobsearch_bot.\nPlease send jobs names seperated by commas.\nIf you would you like to subscribe for a weekly report please send\n/Subscribe\n/help - Print help message"
 
@@ -30,31 +31,42 @@ def manage_sub(message,stmnt):
   conn.close()
 
 
+def chat_log(message):
+  log_stmnt=f'insert into tbl_chat_log(chat_id,chat_text,dtime) values({message.chat.id},"{message.text}","{dtime}")'    
+  manage_sub(message,log_stmnt)
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id,wlcm_msg)
+  print(message.text)
+  chat_log(message)
+  bot.send_message(message.chat.id,wlcm_msg)
+
     
 @bot.message_handler(commands=['Unsubscribe'])
 def unsubscribe(message):
-    print(message.text)
-    mng_sub = f"delete from tbl_subscribers where sub_chat_id = '{message.chat.id}'"
-    manage_sub(message,mng_sub) 
-    bot.send_message(message.chat.id,'Unsubscribed succsesfully\nto subscribe again please send \n/Subscribe')
+  print(message.text)
+  chat_log(message)
+  mng_sub = f"delete from tbl_subscribers where sub_chat_id = '{message.chat.id}'"
+  manage_sub(message,mng_sub) 
+  bot.send_message(message.chat.id,'Unsubscribed succsesfully\nto subscribe again please send \n/Subscribe')
 
     
 @bot.message_handler(commands=['Change'])
 @bot.message_handler(commands=['Subscribe'])
 def subscribe(message):
-    print(message.text)
-    msg = bot.send_message(message.chat.id, "Ok, Please write jobs seperated by commas")
-    bot.register_next_step_handler(msg, ck1)
+  print(message.text)
+  chat_log(message)
+  msg = bot.send_message(message.chat.id, "Ok, Please write jobs seperated by commas")
+  bot.register_next_step_handler(msg, ck1)
 
 def ck1(message):
-    if not message.text.startswith("/"):
-        # mng_sub = f'replace into tbl_subscribers(sub_chat_id,jobs) values({message.chat.id},"{message.text}")'
-        mng_sub = f"INSERT INTO tbl_subscribers (sub_chat_id,jobs) VALUES ({message.chat.id},'{message.text}') ON CONFLICT (sub_chat_id) DO UPDATE SET jobs = excluded.jobs;"
-        manage_sub(message,mng_sub)
-    bot.send_message(message.chat.id,f'"{message.text}"\nis your watch list now,\nto change please send /Change\nto unsubscribe please \nsend /Unsubscribe')
+  if not message.text.startswith("/"):
+    print(message.text)
+    chat_log(message)
+    # mng_sub = f'replace into tbl_subscribers(sub_chat_id,jobs) values({message.chat.id},"{message.text}")'
+    mng_sub = f"INSERT INTO tbl_subscribers (sub_chat_id,jobs) VALUES ({message.chat.id},'{message.text}') ON CONFLICT (sub_chat_id) DO UPDATE SET jobs = excluded.jobs;"
+    manage_sub(message,mng_sub)
+  bot.send_message(message.chat.id,f'"{message.text}"\nis your watch list now,\nto change please send /Change\nto unsubscribe please \nsend /Unsubscribe')
 
 
 
@@ -116,27 +128,28 @@ def send_jobs(message):
   chat_id = message.chat.id
   chat_jobs = message.text.split(',')
   print(chat_id,chat_jobs)
+  chat_log(message)
   search_send_jobs(chat_id,chat_jobs,sites)
 
 
 
 @server.route('/' + API_KEY, methods=['POST'])
 def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
+  json_string = request.get_data().decode('utf-8')
+  update = telebot.types.Update.de_json(json_string)
+  bot.process_new_updates([update])
+  return "!", 200
 
 
 @server.route("/")
 def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://boiling-anchorage-39680.herokuapp.com/' + API_KEY)
-    return "!", 200
+  bot.remove_webhook()
+  bot.set_webhook(url='https://boiling-anchorage-39680.herokuapp.com/' + API_KEY)
+  return "!", 200
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+  server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
 # bot.remove_webhook()
